3136a3fa9edba2287d2541aa651beec1e0993626
yutui <yutui@qq.com>
2020-12-22 22:04:46 +0800

    首页布局  新增tabBar文件

--- lifeRecord/pages/home/home.vue
+++ lifeRecord/pages/home/home.vue
@@ -1,22 +1,34 @@
 <template>
 	<view>
-		<view class="u-relative" style="height: 400rpx;">
+		<view class="u-relative" style="height: 460rpx;">
 			<view class="top-wpr u-relative">
-				<view class="u-flex u-row-right u-p-t-40 u-p-r-30">
+				<view class="status_bar">
+					<!-- 这里是状态栏 -->
+				</view>
+				<view class="u-flex u-row-right u-p-t-30 u-p-r-30">
 					<u-icon name="search" color="#000000" size="50" class="u-m-r-40"></u-icon>
 					<u-icon name="more-dot-fill" color="#000000" size="50" style="transform: rotate(90deg);"></u-icon>
 				</view>
-				<view class="u-flex u-row-between u-p-l-30 u-p-r-30" style="color: #ffffff;">
+				<!-- #ifdef APP-PLUS -->
+					<view class="u-flex u-row-between u-col-bottom u-p-l-50 u-p-r-50 u-m-t-20" style="color: #ffffff;">
+				<!-- #endif -->
+				<!-- #ifdef MP-WEIXIN -->
+					<view class="u-flex u-row-between u-col-bottom u-p-l-50 u-p-r-50 u-m-t-50" style="color: #ffffff;">
+				<!-- #endif -->
+				<!-- #ifdef H5 -->
+					<view class="u-flex u-row-between u-col-bottom u-p-l-50 u-p-r-50 u-m-t-80" style="color: #ffffff;">
+				<!-- #endif -->
 					<view class="">
-						<view style="font-weight: 700;font-size: 90rpx;font-family: KaiTi;">
+						<view class="u-p-l-20" style="font-weight: 700;font-size: 90rpx;font-family: KaiTi;">
 							43
 						</view>
 						<view class="">
-							时长领书卷立即兑换
+							时长领书卷立即兑换 >
 						</view>
 					</view>
-					<view class="">
-						领50书卷
+					<view class="u-text-center">
+						<u-icon name="calendar" size="50" color="#71bcff"></u-icon>
+						<view class="u-font-12">签到领时长</view>
 					</view>
 				</view>
 			</view>
@@ -24,14 +36,14 @@
 				<view class="u-flex rotation-content">
 					<u-image width="40rpx" height="40rpx" mode="scaleToFill" :src="iconBook" style="transform: translate(24rpx,0);"></u-image>
 					<view class="u-flex-1">
-						<u-notice-bar mode="vertical" :volume-icon="false" :more-icon="true" type="none" :list="recommendList"></u-notice-bar>
+						<u-notice-bar mode="vertical" :volume-icon="false" :more-icon="true" type="none" :duration="4000" :list="recommendList"></u-notice-bar>
 					</view>
 				</view>
 			</view>
 		</view>
 		<view class="u-m-t-20 u-p-l-30 u-p-r-30 ">
 			<view class="book-item u-relative u-flex u-m-b-30" v-for="(item,index) in bookList" :key="index" @longtap="longtap(index)">
-				<u-image width="106rpx" height="130rpx" :src="item.avatar" class="u-m-r-20" style="border-radius: 8rpx;overflow: hidden;"></u-image>
+				<u-image width="110rpx" height="146rpx" :src="item.avatar" class="u-m-r-20" style="border-radius: 8rpx;overflow: hidden;"></u-image>
 				<view class="">
 					<view class="u-m-b-20">
 						{{item.name}}
@@ -42,8 +54,8 @@
 				</view>
 				<button class="u-reset-button u-font-12 icon-update" v-show="false">更新</button>
 				<view class="u-flex-1 u-flex u-row-right">
-					<u-checkbox-group >
-						<u-checkbox v-model="item.check" v-show="checked" shape="circle" @change="checkChange"></u-checkbox>
+					<u-checkbox-group>
+						<u-checkbox v-model="item.check" v-show="checked" shape="circle" @change="checkChange(index)"></u-checkbox>
 					</u-checkbox-group>
 				</view>
 			</view>
@@ -55,17 +67,22 @@
 					更多精彩小说
 				</view>
 			</view>
-			
+
 		</view>
-		<view class="u-fixed u-flex" style="left: 0;top: 0;height: 100rpx;background-color: #ffffff;width: 100%;" v-show="checked">
-			<view class="u-flex-1 u-p-l-40" @click="checkAll">
-				{{title}}
+		<view class="u-fixed " style="left: 0;top: 0;background-color: #ffffff;width: 100%;" v-show="checked">
+			<view class="status_bar">
+				<!-- 这里是状态栏 -->
 			</view>
-			<view class="u-flex-1 u-text-center u-font-18" style="color:#000000">
-				书架
-			</view>
-			<view class="u-flex-1 u-text-right u-p-r-40" @click="complete">
-				完成
+			<view class="u-flex" style="height: 100rpx;">
+				<view class="u-flex-1 u-p-l-40" @click="checkAll">
+					{{title}}
+				</view>
+				<view class="u-flex-1 u-text-center u-font-18" style="color:#000000">
+					书架
+				</view>
+				<view class="u-flex-1 u-text-right u-p-r-40" @click="complete">
+					完成
+				</view>
 			</view>
 		</view>
 		<view class="book-select" v-show="checked">
@@ -74,7 +91,7 @@
 					<view class="u-bolder">
 						{{bookDescribe.name}}
 					</view>
-					<view class="u-font-12 u-m-t-10"style="color: #2979ff;">
+					<view class="u-font-12 u-m-t-10" style="color: #2979ff;">
 						查看详情>
 					</view>
 				</view>
@@ -83,11 +100,11 @@
 				</view>
 			</view>
 			<view class="u-flex u-row-between u-text-center u-border-top" style="height: 100rpx; ">
-				<view class="u-flex-1" >
+				<view class="u-flex-1">
 					置顶
 				</view>
-				<view class="u-flex-1" style="color: #fa3534;">
-					删除(1)
+				<view class="u-flex-1" style="color: #fa3534;" @click="bookDelet">
+					删除<text v-show="num > 0">({{num}})</text>
 				</view>
 			</view>
 		</view>
@@ -104,63 +121,114 @@
 					'洛阳亲友如相问',
 					'一片冰心在玉壶'
 				],
-				bookList:[
+				bookList: [
 					{
-						avatar:"https://ggzqimg.cdn.bcebos.com/BookFiles/BookImages/zhangyeyuhuo.jpg",
-						name:"长夜余火",
-						describe:"更新至第569话  正确的选择",
-						check:false,
-						update:true,
+						avatar: "https://bookcover.yuewen.com/qdbimg/349573/1018027842/180",
+						name: "万族之劫",
+						describe: "更新至第861话  回归(万更求订阅)",
+						check: false,
+						update: true,
 					},
 					{
-						avatar:"https://www.miaojiang8.com/files/article/image/6/6233/6233s.jpg",
-						name:"突然成了仙怎么办",
-						describe:"更新至第569话  不正确的选择",
-						check:false,
-						update:true,
+						avatar: "https://bookcover.yuewen.com/qdbimg/349573/1020101696/150",
+						name: "磨了10年剑的我终于可以浪了",
+						describe: "更新至第468话  京城风雨",
+						check: false,
+						update: true,
 					},
+					{
+						avatar: "https://bookcover.yuewen.com/qdbimg/349573/1024062003/150",
+						name: "我在日本当棋圣",
+						describe: "更新至第151话  神秘棋手",
+						check: false,
+						update: true,
+					},
+					{
+						avatar: "https://ggzqimg.cdn.bcebos.com/BookFiles/BookImages/zhangyeyuhuo.jpg",
+						name: "长夜余火",
+						describe: "更新至第569话  正确的选择",
+						check: false,
+						update: true,
+					},
+					{
+						avatar: "https://www.miaojiang8.com/files/article/image/6/6233/6233s.jpg",
+						name: "突然成了仙怎么办",
+						describe: "更新至第569话  不正确的选择",
+						check: false,
+						update: true,
+					},
+					
 				],
-				bookDescribe:{},
-				title:"全选",
+				bookDescribe: {},
+				title: "全选",
 				checked: false,
 				iconBook: require('../../static/icons/book.png'),
 			}
 		},
-		onLoad() {
+		computed: {
+		  num(){
+			let num = 0
+			this.bookList.forEach(v => {
+				if(v.check){
+					num+=1
+				}
+			})
+			return num
+		  }  
 		},
+		onLoad() {},
 		methods: {
 			// 长按显示编辑
-			longtap(i){
-				if(!this.checked){
+			longtap(i) {
+				if (!this.checked) {
 					this.bookList[i].check = true
 					this.checked = true
-					this.checkChange();
 					uni.hideTabBar({})
-					this.bookDescribe =this.$u.deepClone(this.bookList[i]) 	
+					this.bookDescribe = this.$u.deepClone(this.bookList[i])
 				}
 			},
 			// 全选 取消全选
-			checkAll(){
-				if(this.title == '全选'){
-					this.bookList.forEach(v=>{
+			checkAll() {
+				if (this.title == '全选') {
+					this.bookList.forEach(v => {
 						v.check = true
 					})
 					this.title = '取消全选'
-				}else{
-					this.bookList.forEach(v=>{
+				} else {
+					this.bookList.forEach(v => {
 						v.check = false
 					})
 					this.title = '全选'
 				}
 			},
 			// book选中状态
-			checkChange(e){
-				
+			checkChange(e) {
+				this.title = '取消全选'
+				this.bookList.forEach((v, i) => {
+					if (e == i) {
+						if (v.check) {
+							this.title = '全选'
+						}
+					} else if (!v.check) {
+						this.title = '全选'
+					}
+				})
 			},
 			// 完成
-			complete(){
+			complete() {
 				this.checked = false
+				this.bookList.forEach(v => {
+					v.check = false
+				})
 				uni.showTabBar({})
+			},
+			// 删除
+			bookDelet() {
+				for (let i = this.bookList.length - 1; i >= 0; i--) {
+					if (this.bookList[i].check) {
+						this.bookList.splice(i, 1);
+					}
+				}
 			}
 		}
 	}
@@ -169,8 +237,8 @@
 <style lang="scss" scoped>
 	.top-wpr {
 		width: 100%;
-		height: 360rpx;
-		background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-wwgjizeoojbef6e56c/24d77650-41d2-11eb-b997-9918a5dda011.jpg);
+		height: 440rpx;
+		background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(../../static/images/home_bg.jpg);
 		background-size: 100% 100%;
 		border-bottom-right-radius: 600rpx 60rpx;
 		border-bottom-left-radius: 600rpx 60rpx;
@@ -204,7 +272,8 @@
 	.book-describe {
 		color: $u-type-info;
 	}
-	.book-recommend{
+
+	.book-recommend {
 		width: 106rpx;
 		height: 130rpx;
 		border-radius: 8rpx;
@@ -215,7 +284,8 @@
 		font-size: 60rpx;
 		line-height: 114rpx;
 	}
-	.book-select{
+
+	.book-select {
 		position: fixed;
 		left: 0;
 		bottom: 0;
