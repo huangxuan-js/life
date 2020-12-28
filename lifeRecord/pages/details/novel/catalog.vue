a9b8d234baa7b9d8fd911a368d9e787249885933
yutui <yutui@qq.com>
2020-12-28 22:08:11 +0800

    小说目录

---
+++ lifeRecord/pages/details/novel/catalog.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<u-navbar ></u-navbar>
+		<view class="container">
+			<view class="u-p-l-20 u-p-t-30 u-p-b-30 u-border-bottom" :class="{'active' : index == active}" v-for="(item,index) in catalogList" :key="index">
+				{{item.title}}
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				catalogList:[],
+				active:0,
+			}
+		},
+		onLoad(){
+			uni.request({
+			    url: 'http://api.pingcc.cn/fictionChapter/search/127557',
+			    success: (res) => {
+					this.catalogList = res.data.data.data
+			    }
+			});
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.active{
+		color: #24acf2;
+	}
+</style>
